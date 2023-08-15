347af4ff2ba229d0a81cb8bcf02c20e84231921f
lujingwei <lujingwei002@qq.com>
2023-08-15 14:54:21 +0800

    update test

--- proj/resource.go
+++ proj/resource.go
@@ -11,7 +11,7 @@ func LoadResource(ctx context.Context, loader gira.ResourceLoader) error {
 	// 连接resourcedb
 	if resourceDbClient, err := NewResourceDbClient(ctx); err != nil {
 		return err
-	} else if err := loader.LoadResource(ctx, resourceDbClient, path.Join(Dir.ResourceDir, "conf"), true); err != nil {
+	} else if err := loader.LoadResource(ctx, resourceDbClient, path.Join(Dir.ResourceDir, "conf"), false); err != nil {
 		return err
 	} else {
 		return nil
